7a902d7504d26c9aff92adee95716eee879a67d8
Dharani T <gct.it18.dharani@gmail.com>
2021-01-03 20:45:08 +0530

    PHP Application

---
+++ Week 5 First PHP Application/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dharani T PHP</title>
+</head>
+<body>
+<h1>Dharani T PHP</h1>
+<?php
+print "The SHA256 hash of 'Dharani T' is "
+?>
+<?php
+print hash('sha256', 'Dharani T');
+?>
+<pre>ASCII ART:
+    
+    ***********
+    **       **
+    **       **
+    **       **
+    **       **
+    **       **
+    ***********
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
